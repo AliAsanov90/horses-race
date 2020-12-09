2ac859e5d449b25009b216b87a026373d70d9164
Ali Asanov <aliasanov07@gmail.com>
2020-12-09 09:30:16 +0300

    start adding styles of horse-list component

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -4,12 +4,18 @@
     <div class="control-panel__controls">
       <button class="control-panel__button generate" type="button">Generate program</button>
       <button class="control-panel__button start-pause" type="button">Start / Pause</button>
+      <v-btn>button</v-btn>
     </div>
   </div>
 </template>
 
 <script>
+import { VBtn } from 'vuetify/lib'
+
 export default {
-  name: 'ControlPanel'
+  name: 'ControlPanel',
+  components: {
+    VBtn
+  }
 }
 </script>
--- src/components/HorseList.vue
+++ src/components/HorseList.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>HorseList</div>
+  <div class="horse-list">
+    <div class="horse-list__title">Horse List (1-20)</div>
+    <table class="horse-list__table" cellspacing="20px">
+      <thead class="horse-list__table-head">
+        <tr>
+          <th>Name</th>
+          <th>Condition</th>
+          <th>Color</th>
+        </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Adam Loverace</td>
+        <td>80</td>
+        <td>red</td>
+      </tr>
+      <tr>
+        <td>Adam Loverace</td>
+        <td>80</td>
+        <td>red</td>
+      </tr>
+      <tr>
+        <td>Adam Loverace</td>
+        <td>80</td>
+        <td>red</td>
+      </tr>
+    </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
